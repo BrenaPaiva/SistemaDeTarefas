fd0545154ec896b862e25b7cead5cd27660809d8
Brena Paiva <bpaiva@uniaoquimica.com.br>
2023-10-04 19:51:56 -0300

    Finalização CRUD

--- SistemaDeTarefas/Models/UsuarioModel.cs
+++ SistemaDeTarefas/Models/UsuarioModel.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; } 
 
         public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
         public string? Email { get; set; }
 
     }
